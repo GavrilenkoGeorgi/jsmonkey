8fc0d2db848f54044883ebfcc59316fc65f252ff
Georgiy-huma <georgiy.havrylenko@huma.com>
2023-02-03 17:33:56 +0200

    Cleanup reCaptcha verify request

--- utils/reCaptcha.ts
+++ utils/reCaptcha.ts
@@ -1,14 +1,10 @@
 import axios from 'axios'
 
 export const validateToken = (token: string) => {
-  // console.log('Token: ', token)
-  // console.log('backend url ', process.env.NEXT_PUBLIC_CAPTCHA_BACKEND)
 
   const url = `${process.env.NEXT_PUBLIC_CAPTCHA_BACKEND}`
-
   const data = {
-    secret: process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY,
-    response: token
+    token
   }
 
   axios.post(url, data)
