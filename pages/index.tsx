22c20fc189aa46913b8309c19925e8e16ce314a5
Georgiy-huma <georgiy.havrylenko@huma.com>
2023-02-09 16:14:40 +0200

    Add random color to github icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,48 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
 
 import HeroSection from '../components/layout/HeroSection'
 import heroImg from '../assets/images/main-page-pattern.webp'
 import Carousel from '../components/layout/Carousel/Carousel'
 import ContactForm from '../components/forms/ContactForm'
-import downloadIcon from '../assets/icons/icon-download.svg'
-import githubIcon from '../public/img/logos/github.svg'
+import MainPageCTA from '../components/layout/MainPageCTA'
 
 import styles from '../styles/Main.module.scss'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>JSMonkey.dev</title>
-        <meta name="description" content="My resume, projects, blog." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  return <div className={styles.container}>
+    <Head>
+      <title>JSMonkey.dev</title>
+      <meta name="description" content="My resume, projects, blog." />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main className={styles.main}>
-        <HeroSection heading='JSMonkey.dev' image={heroImg} />
-          <section className={styles.containerLg}>
-            <p className={styles.leadParagraph}>
-              Hi, my name is George and I am working as a full-time Middle Frontend Developer.
-            </p>
+    <main className={styles.main}>
+      <HeroSection heading='JSMonkey.dev' image={heroImg} />
+        <section className={styles.containerLg}>
+          <p className={styles.leadParagraph}>
+            Hi, my name is George and I am working as a full-time Middle Frontend Developer.
+          </p>
 
-            <p className={styles.pageParagraph}>
-              Most of my experience is with ECMAScript 6/ES2015, Next.js, React, Redux, Node.js, Express, MongoDB, Webpack 5,
-              HTML5, CSS3, SASS/SCSS. I am testing using Jest and Cypress. My prior experience includes PHP7, MySQL and
-              a basic knowledge of C and Java.
-              My English is Upper Intermediate (B2), I speak Ukrainian and Russian languages.
-              Currently living in Kyiv, can relocate.
-            </p>
+          <p className={styles.pageParagraph}>
+            Most of my experience is with ECMAScript 6/ES2015, Next.js, React, Redux, Node.js, Express, MongoDB, Webpack 5,
+            HTML5, CSS3, SASS/SCSS. I am testing using Jest and Cypress. My prior experience includes PHP7, MySQL and
+            a basic knowledge of C and Java.
+            My English is Upper Intermediate (B2), I speak Ukrainian and Russian languages.
+            Currently living in Kyiv, can relocate.
+          </p>
+          <MainPageCTA />
+        </section>
 
-            <div className={styles.downloadResumeCTA}>
-              <Link href="/pdf/gavrylenkoCV.pdf">
-                My resume in pdf
-                <Image
-                  src={downloadIcon}
-                  alt="Download icon"
-                  width={65}
-                  height={65}
-                  />
-              </Link>
-              <Link href="https://github.com/GavrilenkoGeorgi">
-                Github account
-                <Image
-                  src={githubIcon}
-                  alt="Github icon"
-                  width={40}
-                  height={40}
-                  />
-              </Link>
-            </div>
-          </section>
+        <section className={styles.section}>
+          <Carousel />
+        </section>
 
-          <section className={styles.section}>
-            <Carousel />
-          </section>
-
-          <section className={styles.section}>
-            <ContactForm />
-          </section>
-
-      </main>
-
-    </div>
-  )
+        <section className={styles.section}>
+          <ContactForm />
+        </section>
+    </main>
+  </div>
 }
 
 export default Home
