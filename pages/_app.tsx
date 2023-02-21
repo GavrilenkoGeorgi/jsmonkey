5b46edcea006ae018a07186dc28632a582d59e77
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-02-21 18:07:25 +0200

    Add google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { Noto_Sans } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { ReCaptchaProvider } from 'next-recaptcha-v3'
+import * as gtag from "../utils/gtag"
+import Script from 'next/script'
 
 import '../styles/globals.scss'
 import Layout from '../components/layout/Layout'
@@ -11,7 +15,36 @@ const quicksand = Noto_Sans({
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <div className={quicksand.className}>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-NHDRDGHWQ9"></Script>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NHDRDGHWQ9', {
+            page_path: window.location.pathname,
+          });
+        `,
+      }}
+    />
      <ReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}>
       <Layout>
         <Component {...pageProps} />
