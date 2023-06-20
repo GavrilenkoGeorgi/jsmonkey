de236b42db56f0dfab4e259c233cd915f3488a2c
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-06-20 19:04:50 +0300

    Fix blurry images on mobile

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
-export const SLIDER_IMG_SIZES = '(max-width: 1024px) 100vw, (max-width: 1440px) 50vw, 33vw'
-export const POSTCARD_IMG_SIZES = '(max-width: 820px) 90vw, 40vw'
+export const SLIDER_IMG_SIZES = '(max-width: 1024px) 1200vw, (max-width: 1440px) 1920vw, 3840vw'
+export const POSTCARD_IMG_SIZES = '(max-width: 820px) 828vw, 1920vw'
