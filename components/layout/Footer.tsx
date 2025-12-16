7a1587f74e1e6bebf6346dacbb40f7daf42c58d7
Gosha <gavrilenko.georgi@gmail.com>
2025-12-16 10:39:03 +0200

    Update footer copy

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,38 +1,40 @@
-import { FC } from 'react'
-import Link from 'next/link'
+import { FC } from "react";
+import Link from "next/link";
 
-import Logo from './Logo'
-import SocialIcon from './SocialIcon'
-import facebookIcon from '../../assets/icons/icon-facebook.svg'
-import instagramIcon from '../../assets/icons/icon-instagram.svg'
-import githubIcon from '../../assets/icons/icon-github.svg'
+import Logo from "./Logo";
+import SocialIcon from "./SocialIcon";
+import facebookIcon from "../../assets/icons/icon-facebook.svg";
+import githubIcon from "../../assets/icons/icon-github.svg";
 
-import styles from './Footer.module.sass'
+import styles from "./Footer.module.sass";
 
-const Footer:FC = () => {
-  return <footer className={styles.footer}>
-    <div>
-      <Logo />
-    </div>
-    <div className={styles.socialLinksContainer}>
-      <Link
-        href="/"
-        className={styles.textLink}
-      >
-        I build things.
-      </Link>
-      <SocialIcon
-        image={facebookIcon}
-        link="https://facebook.com/gavrilenko.georgi"
-        altText="Facebook icon."
-      />
-      <SocialIcon
-        image={githubIcon}
-        link="https://github.com/GavrilenkoGeorgi"
-        altText="Github icon."
-      />
-    </div>
-  </footer>
-}
+const Footer: FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <Logo />
+      </div>
+      <div className={styles.socialLinksContainer}>
+        <Link
+          href="/"
+          className={styles.textLink}
+          title="Theo stole my slogan."
+        >
+          <s>I build things.</s> <span>I build experiences.</span>
+        </Link>
+        <SocialIcon
+          image={facebookIcon}
+          link="https://facebook.com/gavrilenko.georgi"
+          altText="Facebook icon."
+        />
+        <SocialIcon
+          image={githubIcon}
+          link="https://github.com/GavrilenkoGeorgi"
+          altText="Github icon."
+        />
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
