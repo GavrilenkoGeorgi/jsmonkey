f5624ff68e73864db7a26eb390f07f8887270bdc
Gosha <gavrilenko.georgi@gmail.com>
2026-03-07 17:02:10 +0200

    Update slider: use embla, some other fixes, cleanup

--- components/layout/Slider/ProjectSlider.tsx
+++ components/layout/Slider/ProjectSlider.tsx
@@ -1,35 +1,61 @@
-import type { FC } from 'react'
-import Image from 'next/image'
-import Slider from 'react-slick'
+import { FC, useEffect, useState } from "react";
+import Image from "next/image";
+import useEmblaCarousel from "embla-carousel-react";
 
-import { projectCarouselProps } from '../../../types'
-import { SLIDER_IMG_SIZES } from '../../../utils/constants'
-import styles from './ProjectSlider.module.sass'
+import { projectCarouselProps } from "../../../types";
+import { SLIDER_IMG_SIZES } from "../../../utils/constants";
+import styles from "./ProjectSlider.module.sass";
 
-const ProjectSlider: FC<projectCarouselProps> = ({ title, images, priority }) => {
+const ProjectSlider: FC<projectCarouselProps> = ({
+  title,
+  images,
+  priority,
+}) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const settings = {
-    dots: true,
-    arrows: false
-  }
+  useEffect(() => {
+    if (!emblaApi) return;
+    const onSelect = () => setSelectedIndex(emblaApi.selectedScrollSnap());
+    emblaApi.on("select", onSelect);
+    return () => {
+      emblaApi.off("select", onSelect);
+    };
+  }, [emblaApi]);
 
-  return <Slider {...settings}>
-    {images.map((url, index) => (
-      <div key={url} className={styles.swipeItem}>
-        <div className={styles.imgBox}>
-          <Image
-            src={url}
-            alt={`${title} slide ${index}`}
-            width={1170}
-            height={2532}
-            priority={priority}
-            sizes={SLIDER_IMG_SIZES}
-            className={styles.slide}
-          />
+  return (
+    <div>
+      <div className={styles.viewport} ref={emblaRef}>
+        <div className={styles.slideContainer}>
+          {images.map((url, index) => (
+            <div key={url} className={styles.slide}>
+              <div className={styles.imgBox}>
+                <Image
+                  src={url}
+                  alt={`${title} slide ${index}`}
+                  width={1170}
+                  height={2532}
+                  priority={priority}
+                  sizes={SLIDER_IMG_SIZES}
+                  className={styles.slideImg}
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      ))}
-  </Slider>
-}
+      <div className={styles.dots}>
+        {images.map((_, index) => (
+          <button
+            key={index}
+            className={`${styles.dot} ${index === selectedIndex ? styles.dotActive : ""}`}
+            onClick={() => emblaApi?.scrollTo(index)}
+            aria-label={`Go to slide ${index + 1}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default ProjectSlider
+export default ProjectSlider;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import * as gtag from "../utils/gtag";
 import { ThemeProvider } from "../context/ThemeContext";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import "../styles/globals.scss";
 import Layout from "../components/layout/Layout";
