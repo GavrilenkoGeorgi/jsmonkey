ff2ba8091cc34c8ffa040f11882a7278fc0224cf
Gosha <gavrilenko.georgi@gmail.com>
2026-03-08 17:22:41 +0200

    Update blog page
    - Updated animations
    - Add tags to blog posts

--- components/blog/ListOfPosts.tsx
+++ components/blog/ListOfPosts.tsx
@@ -1,29 +1,56 @@
-import { FC } from 'react'
-import { format } from 'date-fns'
-import { blogProps } from '../../types'
+import { FC, useEffect } from "react";
+import { format } from "date-fns";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { blogProps } from "../../types";
 
-import PostCard from './PostCard'
-import styles from './ListOfPosts.module.sass'
+import PostCard from "./PostCard";
+import styles from "./ListOfPosts.module.sass";
+
+const containerVariants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.6,
+    },
+  },
+};
+
+const cardVariants = {
+  hidden: { opacity: 0, y: "25%" },
+  visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+};
 
 const ListOfPosts: FC<blogProps> = ({ posts }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
 
-  const listOfPosts = posts.map(post =>
-    <PostCard
-      key={post.slug}
-      slug={post.slug}
-      date={format(new Date(post.date), 'MMMM do yyyy')}
-      title={post.title}
-      body={post.body}
-      image={post.image}
-      author={post.author}
-    />
-  )
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
 
-  return <aside className={styles.postsListSection}>
-    <div className={styles.listContainer}>
-      {listOfPosts}
-    </div>
-  </aside>
-}
+  return (
+    <section className={styles.postsListSection}>
+      <motion.ul
+        className={styles.listContainer}
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={containerVariants}
+      >
+        {posts.map((post) => (
+          <motion.li key={post.slug} variants={cardVariants}>
+            <PostCard
+              {...post}
+              date={format(new Date(post.date), "MMMM do yyyy")}
+            />
+          </motion.li>
+        ))}
+      </motion.ul>
+    </section>
+  );
+};
 
-export default ListOfPosts
+export default ListOfPosts;
