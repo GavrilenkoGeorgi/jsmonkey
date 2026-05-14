3037361e95a4aee6960ea1c20893645d15ef84a0
Gosha <gavrilenko.georgi@gmail.com>
2026-05-14 15:24:41 +0300

    Fix accessibility issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,11 +27,6 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
-        id="netlify-manager"
-        strategy="afterInteractive"
-        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-      />
-      <Script
         id="gtag-manager"
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
@@ -47,23 +42,6 @@ function App({ Component, pageProps }: AppProps) {
             gtag('config', '${gtag.GA_MEASUREMENT_ID}', {
               page_path: window.location.pathname,
             });
-          `,
-        }}
-      />
-      <Script
-        id="netlify-ident"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            if (window.netlifyIdentity) {
-              window.netlifyIdentity.on('init', user => {
-                if (!user) {
-                  window.netlifyIdentity.on('login', () => {
-                    document.location.href = '/admin/';
-                  });
-                }
-              });
-            }
           `,
         }}
       />
